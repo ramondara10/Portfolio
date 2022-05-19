acb21f0942c9c96df0f126b5b0054a65179ed18f
ramondara10 <68656067+ramondara10@users.noreply.github.com>
2022-05-19 16:28:23 -0500

    working on the about component

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import './about.css'
 import ME from '../../assests/me-about.jpg'
 import {FaAward} from 'react-icons/fa'
+import {FiUsers} from 'react-icons/fi'
+import {VscFolderLibrary} from 'react-icons/vsc'
 
 const About = () => {
   return (
@@ -28,19 +30,25 @@ const About = () => {
 
           <div className="about__cards">
             <article className='about__card'>
-              <FaAward className='about__icon'/>
-              <h5>Experience</h5>
-              <small>Entry Level</small> 
+              <FiUsers className='about__icon'/>
+              <h5>Clients</h5>
+              <small>None</small> 
             </article>
           </div>
 
           <div className="about__cards">
             <article className='about__card'>
-              <FaAward className='about__icon'/>
-              <h5>Experience</h5>
-              <small>Entry Level</small> 
+              <VscFolderLibrary className='about__icon'/>
+              <h5>Projects</h5>
+              <small>20+ Completed Projects</small> 
             </article>
           </div>
+          <p>
+            I graduated from SMU as a Fullstack Developer the year 2022. I have experience working
+            with Mongo DB, Express, React and Node.js. I've also worked with HTML, CSS, JS and Redux.
+            I have knowledge in GraphQL and MySQL. I may lack the experience but I do enjoy coding and I will learn
+            from my mentors and peers in order to provide the best work I can. 
+          </p>
 
         </div>
       </div>
