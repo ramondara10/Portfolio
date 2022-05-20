296d67d6cb19138d7fb20f05fe16ae04f337b9b1
ramondara10 <68656067+ramondara10@users.noreply.github.com>
2022-05-20 16:22:51 -0500

    started working on the services component

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,9 +1,46 @@
 import React from 'react'
 import './services.css'
+import {BiCheck} from 'react-icons/bi'
 
 const Services = () => {
   return (
-    <section id='services'>Services</section>
+    <section id='services'>
+      <h5>
+        What I Offer
+      </h5>
+      <h2>
+        Services
+      </h2>
+      <div className="container services__container">
+        <article className='service'>
+          <div className="service__head">
+            <h3>Web Development</h3>
+          </div>
+          <ul className='service__list'>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p> Ecommerce</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Front-End Design and Development</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Back-end Development</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p></p>
+            </li>
+          </ul>
+
+        </article>
+        
+        {/*End of Web Development when more skills are acquired you can use the same code to add more in different categories*/}
+
+      </div>
+    </section>
   )
 }
 
