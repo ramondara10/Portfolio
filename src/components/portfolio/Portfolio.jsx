8ea740c78e3e680bb268d65d14a9b6d9d571b0b3
ramondara10 <68656067+ramondara10@users.noreply.github.com>
2022-05-23 16:13:56 -0500

    worked on the portfolio component

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,5 +1,12 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../assests/portfolio1'
+import IMG2 from '../../assests/portfolio2'
+import IMG3 from '../../assests/portfolio3'
+import IMG4 from '../../assests/portfolio4'
+import IMG5 from '../../assests/portfolio5'
+import IMG6 from '../../assests/portfolio6'
+
 
 const Portfolio = () => {
   return (
@@ -9,7 +16,70 @@ const Portfolio = () => {
 
       <div className="container portfolio__container">
         <article className='portfolio__item'>
-          <div className="portfolio__item-image"></div>
+          <div className="portfolio__item-image">
+
+          <img src={IMG1} alt="" />
+
+          </div>
+          <h3>This is Portfolio Title</h3>
+          <a href="https://github.com/ramondara10/Tesla-Clone" className='btn' target='_blank'>Github</a>
+          <a href="https://ramondara10.github.io/Tesla-Clone/" className='btn btn-primary' target='_blank'> Live Demo</a>
+
+        </article>
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+
+          <img src={IMG1} alt="" />
+
+          </div>
+          <h3>This is Portfolio Title</h3>
+          <a href="https://github.com/ramondara10/Tesla-Clone" className='btn' target='_blank'>Github</a>
+          <a href="https://ramondara10.github.io/Tesla-Clone/" className='btn btn-primary' target='_blank'> Live Demo</a>
+
+        </article>
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+
+          <img src={IMG1} alt="" />
+
+          </div>
+          <h3>This is Portfolio Title</h3>
+          <a href="https://github.com/ramondara10/Tesla-Clone" className='btn' target='_blank'>Github</a>
+          <a href="https://ramondara10.github.io/Tesla-Clone/" className='btn btn-primary' target='_blank'> Live Demo</a>
+
+        </article>
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+
+          <img src={IMG1} alt="" />
+
+          </div>
+          <h3>This is Portfolio Title</h3>
+          <a href="https://github.com/ramondara10/Tesla-Clone" className='btn' target='_blank'>Github</a>
+          <a href="https://ramondara10.github.io/Tesla-Clone/" className='btn btn-primary' target='_blank'> Live Demo</a>
+
+        </article>
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+
+          <img src={IMG1} alt="" />
+
+          </div>
+          <h3>This is Portfolio Title</h3>
+          <a href="https://github.com/ramondara10/Tesla-Clone" className='btn' target='_blank'>Github</a>
+          <a href="https://ramondara10.github.io/Tesla-Clone/" className='btn btn-primary' target='_blank'> Live Demo</a>
+
+        </article>
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+
+          <img src={IMG1} alt="" />
+
+          </div>
+          <h3>This is Portfolio Title</h3>
+          <a href="https://github.com/ramondara10/Tesla-Clone" className='btn' target='_blank'>Github</a>
+          <a href="https://ramondara10.github.io/Tesla-Clone/" className='btn btn-primary' target='_blank'> Live Demo</a>
+
         </article>
       </div>
     </section>
