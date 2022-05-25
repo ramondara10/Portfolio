5d05b5e0e276889637f388a417408b7882246ccd
ramondara10 <68656067+ramondara10@users.noreply.github.com>
2022-05-25 15:39:30 -0500

    worked on the testimonials component fixed minor issues

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -5,78 +5,67 @@ import AVTR2 from '../../assests/avatar2.jpg'
 import AVTR3 from '../../assests/avatar3.jpg'
 import AVTR4 from '../../assests/avatar4.jpg'
 
+// import Swiper core and required modules
+import { Pagination } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+
 const data = [
   {
     avatar: AVTR1,
-    name: 'Tina Snow',
-    review: 'Modi alias animi dolorem aliquam ea eum beatae maiores, consectetur praesentium quibusdam, commodi velit porro blanditiis consequatur qui molestiae. Dolorem, perspiciatis aspernatur labore distinctio ratione delectus voluptatem dolores deserunt explicabo nostrum ducimus quasi?'
+    name: 'Testimonial Section',
+    review: 'This section is  placeholder for actual reviews. Currently I do not have any any reviews so it serves as a showcase.'
   },
   {
     avatar: AVTR2,
-    name: 'Shatta Wale',
+    name: 'John Smith',
     review: 'Modi alias animi dolorem aliquam ea eum beatae maiores, consectetur praesentium quibusdam, commodi velit porro blanditiis consequatur qui molestiae. Dolorem, perspiciatis aspernatur labore distinctio ratione delectus voluptatem dolores deserunt explicabo nostrum ducimus quasi?'
   },
   {
     avatar: AVTR3,
-    name: 'Kwame Despite',
+    name: 'John Doe',
     review: 'Modi alias animi dolorem aliquam ea eum beatae maiores, consectetur praesentium quibusdam, commodi velit porro blanditiis consequatur qui molestiae. Dolorem, perspiciatis aspernatur labore distinctio ratione delectus voluptatem dolores deserunt explicabo nostrum ducimus quasi?'
   },
   {
     avatar: AVTR4,
-    name: 'Nana Ama McBrown',
-    review: 'Modi alias animi dolorem aliquam ea eum beatae maiores, consectetur praesentium quibusdam, commodi velit porro blanditiis consequatur qui molestiae. Dolorem, perspiciatis aspernatur labore distinctio ratione delectus voluptatem dolores deserunt explicabo nostrum ducimus quasi?'
+    name: 'Testimonial Section',
+    review: 'This section is  placeholder for actual reviews. Currently I do not have any any reviews so it serves as a showcase.'
   },
 ]
+
 
 
 
 const Testimonials = () => {
   return (
     <section id='testimonials'>
-      <h5>Reviews</h5>
+      <h5>Review from clients</h5>
       <h2>Testimonials</h2>
-      <div className="conatiner testimonials__container">
-        <article className='testimonial'>
-          <div className="client__avatar">
-            <img src={AVTR1} alt="Avatar image" />
-          </div>
 
-          <h5>Testimonial Section</h5>
-            <small className='client__review'>
-              This section is  placeholder for actual reviews. Currently I do not have any 
-              any reviews so it serves as a showcase.
-
-            </small>
-          
-        </article>
-        <article className='testimonial'>
-          <div className="client__avatar">
-            <img src={AVTR1} alt="Avatar image" />
-          </div>
-
-          <h5>Testimonial Section</h5>
-            <small className='client__review'>
-              This section is  placeholder for actual reviews. Currently I do not have any 
-              any reviews so it serves as a showcase.
-
-            </small>
-          
-        </article>
-        <article className='testimonial'>
-          <div className="client__avatar">
-            <img src={AVTR1} alt="Avatar image" />
-          </div>
-
-          <h5>Testimonial Section</h5>
-            <small className='client__review'>
-              This section is  placeholder for actual reviews. Currently I do not have any 
-              any reviews so it serves as a showcase.
-
-            </small>
-          
-        </article>
-  
-      </div>
+      <Swiper className="container testimonials__container" 
+      // install Swiper modules
+      modules={[Pagination]} spaceBetween={40}
+      slidesPerView={1}
+      pagination={{ clickable: true }}>
+        {
+          data.map(({avatar, name, review}, index) => {
+            return (
+              <SwiperSlide key={index} className="testimonial">
+              <div className="client__avatar">
+                <img src={avatar}/>
+              </div>
+              <h5 className='client__name'>{name}</h5>
+              <small className='client__review'>{review}</small>
+            </SwiperSlide>
+            )
+          })
+        }
+      </Swiper>
     </section>
   )
 }
