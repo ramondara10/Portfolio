2ffdf941c64456639154e79e300d898c6f280785
ramondara10 <68656067+ramondara10@users.noreply.github.com>
2022-05-25 15:05:24 -0500

    working on testimonials component

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -5,6 +5,30 @@ import AVTR2 from '../../assests/avatar2.jpg'
 import AVTR3 from '../../assests/avatar3.jpg'
 import AVTR4 from '../../assests/avatar4.jpg'
 
+const data = [
+  {
+    avatar: AVTR1,
+    name: 'Tina Snow',
+    review: 'Modi alias animi dolorem aliquam ea eum beatae maiores, consectetur praesentium quibusdam, commodi velit porro blanditiis consequatur qui molestiae. Dolorem, perspiciatis aspernatur labore distinctio ratione delectus voluptatem dolores deserunt explicabo nostrum ducimus quasi?'
+  },
+  {
+    avatar: AVTR2,
+    name: 'Shatta Wale',
+    review: 'Modi alias animi dolorem aliquam ea eum beatae maiores, consectetur praesentium quibusdam, commodi velit porro blanditiis consequatur qui molestiae. Dolorem, perspiciatis aspernatur labore distinctio ratione delectus voluptatem dolores deserunt explicabo nostrum ducimus quasi?'
+  },
+  {
+    avatar: AVTR3,
+    name: 'Kwame Despite',
+    review: 'Modi alias animi dolorem aliquam ea eum beatae maiores, consectetur praesentium quibusdam, commodi velit porro blanditiis consequatur qui molestiae. Dolorem, perspiciatis aspernatur labore distinctio ratione delectus voluptatem dolores deserunt explicabo nostrum ducimus quasi?'
+  },
+  {
+    avatar: AVTR4,
+    name: 'Nana Ama McBrown',
+    review: 'Modi alias animi dolorem aliquam ea eum beatae maiores, consectetur praesentium quibusdam, commodi velit porro blanditiis consequatur qui molestiae. Dolorem, perspiciatis aspernatur labore distinctio ratione delectus voluptatem dolores deserunt explicabo nostrum ducimus quasi?'
+  },
+]
+
+
 
 const Testimonials = () => {
   return (
