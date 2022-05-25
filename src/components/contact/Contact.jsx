a2f5b7228ec1a4384cff350761cd852ef9bea3c6
ramondara10 <68656067+ramondara10@users.noreply.github.com>
2022-05-25 16:05:09 -0500

    worked on the testimonials component and now currently working on the contact component

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
 import './contact.css'
+import { MdOutlineEmail } from 'react-icons/md'
 
-function Contact() {
+const Contact = () => {
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get in Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__option">
+          <article className='contact__options'>
+            <MdOutlineEmail/>
+            <h4>Email</h4>
+            <h5>tonyalvarado199407@gmail.com</h5>
+            <a href="mailto: tonyalvarado199407@gmail.com">Send a Message</a>
+          </article>
+          <article className='contact__options'>
+            <MdOutlineEmail/>
+            <h4>Messenger</h4>
+            <h5>Ramon Alvarado</h5>
+            <a href="mailto: tonyalvarado199407@gmail.com">Send a Message</a>
+          </article>
+          <article className='contact__options'>
+            <MdOutlineEmail/>
+            <h4>Email</h4>
+            <h5>tonyalvarado199407@gmail.com</h5>
+            <a href="mailto: tonyalvarado199407@gmail.com">Send a Message</a>
+          </article>
+        </div>
+
+      {/*End of contact*/}
+
+      <form action=""></form>
+
+      </div>
+
+    </section>
   )
 }
 
