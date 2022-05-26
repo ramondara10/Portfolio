a31f9a5db7e612c885632c710986f766350e4adf
ramondara10 <68656067+ramondara10@users.noreply.github.com>
2022-05-26 15:40:07 -0500

    wroked on the contact component and styling currently working in the media queries

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './contact.css'
 import { MdOutlineEmail } from 'react-icons/md'
+import { RiMessengerLine } from 'react-icons/ri'
+import { BsWhatsapp } from 'react-icons/bs'
 
 const Contact = () => {
   return (
@@ -11,28 +13,33 @@ const Contact = () => {
       <div className="container contact__container">
         <div className="contact__option">
           <article className='contact__options'>
-            <MdOutlineEmail/>
+            <MdOutlineEmail className='contact__option-icon'/>
             <h4>Email</h4>
             <h5>tonyalvarado199407@gmail.com</h5>
-            <a href="mailto: tonyalvarado199407@gmail.com">Send a Message</a>
+            <a href="mailto: tonyalvarado199407@gmail.com" target='_blank'>Send a Message</a>
           </article>
           <article className='contact__options'>
-            <MdOutlineEmail/>
+            <RiMessengerLine className='contact__option-icon'/>
             <h4>Messenger</h4>
             <h5>Ramon Alvarado</h5>
-            <a href="mailto: tonyalvarado199407@gmail.com">Send a Message</a>
+            <a href="https://m.me/ramonantonio.alvaradorivera.9" target='_blank'>Send a Message</a>
           </article>
           <article className='contact__options'>
-            <MdOutlineEmail/>
-            <h4>Email</h4>
-            <h5>tonyalvarado199407@gmail.com</h5>
-            <a href="mailto: tonyalvarado199407@gmail.com">Send a Message</a>
+            <BsWhatsapp className='contact__option-icon'/>
+            <h4>WhatApp</h4>
+            <h5>+1 469-216-1208</h5>
+            <a href="https://api.whatsapp.com/send?phone=+14692161208" target='_blank'>Send a Message</a>
           </article>
         </div>
 
       {/*End of contact*/}
 
-      <form action=""></form>
+      <form action="">
+          <input type='text' name='name' placeholder='Your Full Name' required/>
+          <input type='email' name='email' placeholder='Your Email' required/>
+          <textarea name="message" rows="7" placeholder='Your Message ' required></textarea>
+          <button type='submit'className='btn btn-primary'>Send Message</button>
+      </form>
 
       </div>
 
