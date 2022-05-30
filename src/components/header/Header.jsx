882ab8eb50f66666c3061d85294d422d93b613ff
ramondara10 <68656067+ramondara10@users.noreply.github.com>
2022-05-30 11:33:01 -0500

    fixed some minor issues with the portfolio need to upload the work for ui/ux side and the projects for web dev after that deployment will happen

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,25 +7,19 @@ import HeaderSocials from './HeaderSocials'
 const Header =()=> {
   return (
     <header>
-        <div className="container header__container">
-            <h5>
-                Hello I'm
-            </h5>
-            <h1>
-                Ramon Alvarado
-            </h1>
-            <h5 className="text-light">
-                Fullstack Developer
-            </h5>
-            <CTA/>
-            <HeaderSocials/>
-            <div className="me">
-                <img src={ME} alt="Me" />
-            </div>
+         <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Ramon Alvarado</h1>
+        <h5 className="text-light">Fullstack Developer and UX/UI Designer</h5>
+        <CTA />
+        <HeaderSocials />
 
-            <a href="#contact" className='scroll__down'> Scroll Down</a>
-            
+        <div className="me">
+          <img src={ME} alt="me" />
         </div>
+
+        <a href="#contact" className='scroll__down'>Scroll Down</a>
+      </div>
     </header>
   )
 }
